3625f73cdbecc81c3dc6ef944fcf52626bd00fa3
Johannes Roth <johannes.roth18199@gmail.com>
2017-10-22 04:46:57 +0200

    Changed to lamba expression

--- src/com/naronco/infinityjam/ExitStepArea.java
+++ src/com/naronco/infinityjam/ExitStepArea.java
@@ -15,12 +15,8 @@ public class ExitStepArea implements StepArea {
 	@Override
 	public boolean stepOn(int x, int y) {
 		if (area.intersects(x, y)) {
-			Game.instance.setScene(transitionTo, new ISceneTransition() {
-				@Override
-				public void render(Screen screen, double time) {
-					screen.renderRectangle(0, (int)(-86 + 86 * 2 * time), 200, 86, 0x121212);
-				}
-			});
+			Game.instance.setScene(transitionTo,
+					(Screen screen, double time) -> screen.renderRectangle(0, (int)(-86 + 86 * 2 * time), 200, 86, 0x121212));
 			return true;
 		}
 		return false;
